2093106de57d969f4c412576d71af92efb5c45ce
compbu20231 <compbu20231@gmail.com>
2023-02-26 12:33:45 +0800

    Lab 5

--- chatbot.py
+++ chatbot.py
@@ -1,7 +1,8 @@
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
-import configparser
+# import configparser
+import os
 import logging
 
 import redis
@@ -10,13 +11,20 @@ global redis1
 
 def main():
     # Load your token and create an Updater for your Bot
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
+    # config = configparser.ConfigParser()
+    # config.read('config.ini')
+    # updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
+    # dispatcher = updater.dispatcher
+    # global redis1
+    # redis1 = redis.Redis(host=(config['REDIS']['HOST']), password=(config['REDIS']
+    # ['PASSWORD']), port=(config['REDIS']['REDISPORT']))
+    updater = Updater(token=(os.environ['ACCESS_TOKEN']), use_context=True)
     dispatcher = updater.dispatcher
     global redis1
-    redis1 = redis.Redis(host=(config['REDIS']['HOST']), password=(config['REDIS']
-    ['PASSWORD']), port=(config['REDIS']['REDISPORT']))
+    redis1 = redis.Redis(host=(os.environ['HOST']), password=
+    (os.environ['PASSWORD']), port=(os.environ['REDISPORT']))
+
+
     # You can set this logging module, so you will know when and why things do not work as expected
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
     # register a dispatcher to handle message: here we register an echo dispatcher
